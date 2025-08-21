ddd060927b3ed4cc25af5ce724e37afde34fd8b5
rodrigofernandezrefg-jpg <rodrigo.fernandez.refg@gmail.com>
2025-08-21 20:52:03 -0300

    Update Ocnitorrinco.java

--- Ocnitorrinco/src/ocnitorrinco/Ocnitorrinco.java
+++ Ocnitorrinco/src/ocnitorrinco/Ocnitorrinco.java
@@ -15,7 +15,8 @@ public class Ocnitorrinco {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
+
