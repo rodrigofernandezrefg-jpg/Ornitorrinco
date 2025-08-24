8d25f7d6f9717ebbcb59a7afb39113037be78fc1
Kjellfrid Bjerkeli <kjellfrid.18@gmail.com>
2025-08-24 16:32:33 -0300

    aqui denuevo....empece el main con instancias

--- Ocnitorrinco/src/ocnitorrinco/OrniMain.java
+++ Ocnitorrinco/src/ocnitorrinco/OrniMain.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author DOMENE =) 
+ * @author Grupo2
  */
 public class OrniMain {
 
@@ -18,7 +18,29 @@ public class OrniMain {
       
       Try{
         System.out.println("Ingrese longitud de cola del OrniVerde:");
-        
+        double colaVerde = sc.nextDouble();
+            System.out.println("Ingrese velocidad del OrniVerde:");
+            int velVerde = sc.nextInt();
+            Orniverde verde = new Orniverde(colaVerde, velVerde);
+            
+            System.out.println("Ingrese longitud de cola del OrniAzul Blue:");
+            double colaBlue = sc.nextDouble();
+            System.out.println("Ingrese velocidad de Blue:");
+            int velBlue = sc.nextInt();
+            Orniazul blue = new Orniazul(colaBlue, velBlue);
+            
+             System.out.println("Ingrese longitud de cola del OrniAzul Blui:");
+            double colaBlui = sc.nextDouble();
+            System.out.println("Ingrese velocidad de Blui:");
+            int velBlui = sc.nextInt();
+            Orniazul blui = new Orniazul(colaBlui, velBlui);
+            
+             System.out.println("\n--- Habilidades del OrniVerde (Grupo2) ---");
+            verde.nadar();
+            verde.tocarGuitarra();
+            verde.tocarOrgano();
+            verde.tocarGuitorgan();
+
          
     }
     }
